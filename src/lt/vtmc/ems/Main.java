983395dbdd286f32ed79ec8aac85bf34dd8907eb
Albert Pilecki <albert.pilecki@gmail.com>
2020-05-12 19:34:43 +0300

    Main faile buvo pakeista is String... i String[]

--- src/lt/vtmc/ems/Main.java
+++ src/lt/vtmc/ems/Main.java
@@ -2,7 +2,7 @@ package lt.vtmc.ems;
 
 public class Main {
 
-    public static void main(String... args) throws Exception {
+    public static void main(String[] args) throws Exception {
         // TODO
         AppMenu menu = new AppMenu();
         menu.mainMenu();
